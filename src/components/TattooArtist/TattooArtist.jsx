040f45e80723ef921cc8c2a955121b9e7dae350e
Steve g <119070310+SteeveeG@users.noreply.github.com>
2024-07-15 10:07:44 +0200

    new Style in Picture Gallery

--- src/components/TattooArtist/TattooArtist.jsx
+++ src/components/TattooArtist/TattooArtist.jsx
@@ -1,41 +1,55 @@
-import React, { useEffect } from 'react'
-import css from './TattooArtist.module.css'
-import example from '../../Assets/Example.png'
-import { useTranslation } from "react-i18next";
-
-
+import React from 'react';
+import css from './TattooArtist.module.css';
+import example from '../../Assets/Example.png';
+import { useTranslation } from 'react-i18next';
 
 function TattooArtist() {
-     const { t } = useTranslation();
-  
-     return (
-          <div className={css.box}>
-               <div>
-                <p className={css.header}>{t("TattooArtists")}</p>
-                     <div className={css.row}>
-                              <div className={css.column}>
-                                   <img src={example} className={css.Pic} style={{height: '500px' }} alt="example pic" />
-                              </div>
-                         <div className={css.column}>
-                                                                                
-                              <img src={example} className={css.Pic} style={{width: '400px', height:'100px' }} alt="example pic" />
-                         </div>
+    const { t } = useTranslation();
 
-                         <div className={css.column}>
-                              <img src={example} className={css.Pic} alt="example pic" />
-                         
-                         </div>
+    return (
+        <div className={css.box}>
+            <p className={css.header}>{t("TattooArtists")}</p>
+            <div className={css.row}>
+                <div className={css.column}>
+                    <img src={example} alt="example pic"/>
+                    <img src={example} alt="example pic"/>
+                    <img src={example} alt="example pic"/>
+                    <p className={css.tattooArtist}>Tattowierer Nummer 2</p>
+                    <img src={example} alt="example pic"/>
+                    <img src={example} alt="example pic"/>
+                    <img src={example} alt="example pic"/>
+                </div>
 
-                         <div className={css.column}>
-                         
-                              <img src={example} className={css.Pic} alt="example pic" />
-                         </div>
-                    </div>
-               </div>
+                <div className={css.column}>
+                <p className={css.tattooArtist}>Tattowierer Nummer 1</p>
+                    <img src={example} alt="example pic"/>
+                    <img src={example} alt="example pic"/>
+                    <img src={example} alt="example pic"/>
+                    <img src={example} alt="example pic"/>
+                    <img src={example} alt="example pic"/>
+                    <img src={example} alt="example pic"/>
+                </div>
 
-          </div>
+                <div className={css.column}>
+                <img src={example} alt="example pic"/>
+                    <img src={example} alt="example pic"/>
+                    <img src={example} alt="example pic"/>
+                    <img src={example} alt="example pic"/>
+                    <img src={example} alt="example pic"/>
+                    <img src={example} alt="example pic"/>
+                </div>
 
-     )
+                <div className={css.column}>
+                    <img src={example} alt="example pic"/>
+                    <img src={example} alt="example pic"/>
+                    <img src={example} alt="example pic"/>
+                    <img src={example} alt="example pic"/>
+                    <img src={example} alt="example pic" />
+                    <img src={example} alt="example pic" />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default TattooArtist;
