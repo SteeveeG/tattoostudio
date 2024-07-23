5c3fb85591c9cd43296704516bd990050b263244
Steven Kühnl <stevnkuehnl@gmail.com>
2024-07-23 18:22:46 +0200

    new component footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,33 @@
+import react from 'react'
+import css from './Footer.module.css'
+import { useTranslation } from "react-i18next";
+
+
+function Footer() {
+     const { t }=useTranslation ();
+     return (
+          <footer className={css.container}>
+               <p className={css.header}>Your Tattoo Studio</p>
+               <div className={css.linkscontainer}>
+                    <ul className={css.categorylist}>
+                         <li className={css.link}><a href="#">{t("tattooartists")}</a></li>
+                         <li className={css.link}><a href="#">{t("contact")}</a></li>
+                         <li className={css.link}><a href="#">{t("location")}</a></li>
+                    </ul>
+                    <ul className={css.tattooartistList}>
+                         <li className={css.link}><a href="#">samuel</a></li>
+                         <li className={css.link}><a href="#">lenni</a></li>
+                         <li className={css.link}><a href="#">john</a></li>
+                         <li className={css.link}><a href="#">isabell</a></li>
+                         <li className={css.link}><a href="#">emma</a></li>
+                         <li className={css.link}><a href="#">whatsapp</a></li>
+                         <li className={css.link}><a href="#">instagram</a></li>
+
+                    </ul>
+               </div>
+               <p className={css.copyright}>Copyright © Your Tattoo Studio</p>
+          </footer>
+     )
+}
+
+export default Footer
